a79c993e64ead943f7cfcd8a2dd8dbedf836a8e5
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-12-16 16:01:11 -0500

    Add styling and basic callout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Callout type="error" title="Something went wrong">
+      <p>
+        It appears that the custom element isn't being loaded in Kentico
+        Kontent.
+      </p>
+    </Callout>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Callout from "./components/Callout";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Callout
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
